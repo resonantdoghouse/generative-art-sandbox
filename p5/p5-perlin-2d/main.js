29a07da85e97eac4ffdb45a6457c058a651dc44d
Jim <jim@advancebass.com>
2022-01-25 06:35:20 -0800

    add perlin line with circle, playing around

--- p5/p5-perlin-2d/main.js
+++ p5/p5-perlin-2d/main.js
@@ -3,7 +3,8 @@ import noiseLines from './sketches/noiseLines';
 import perlinMover from './sketches/perlinMover';
 import graph from './sketches/graph';
 import movingGraph from './sketches/movingGraph';
+import movingGraphWithCircle from './sketches/movingGraphWithCircle';
 import randomPixels from './sketches/randomPixels';
 import './style.css';
 
-new p5(randomPixels);
+new p5(movingGraphWithCircle);
