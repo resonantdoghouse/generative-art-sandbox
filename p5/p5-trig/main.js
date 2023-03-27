e10dc3bf1ead490619462341dbc2cbd345d85cb0
Jim <jim@advancebass.com>
2023-03-26 22:34:34 -0700

    add dat gui to p5 trig

--- p5/p5-trig/main.js
+++ p5/p5-trig/main.js
@@ -1,46 +1,84 @@
-import p5 from 'p5';
-import Circle from './Circle';
-import './style.css';
+import * as dat from 'dat.gui'
+import p5 from 'p5'
+import './style.css'
+
+const settings = {
+  fps: 5,
+  lineWidth: 3,
+  lineHeight: 0.3,
+  angle: 2.4
+}
+
+const gui = new dat.GUI()
+const lineSettings = gui.addFolder('Settings')
 
 const sketch = (s) => {
-  let a = 0.0;
-  let inc = s.TWO_PI / 25.0;
+  lineSettings
+    .add(settings, 'fps', 0, 60)
+    .onChange((value) => {
+      settings.fps = value
+      s.frameRate(settings.fps)
+    })
+    .step(1)
 
-  const list_of_colors = ['skyblue', 'darkturquoise', 'aquamarine'];
+    lineSettings
+    .add(settings, 'lineWidth', 1, 20)
+    .onChange((value) => {
+      settings.lineWidth = value
+    })
+    .step(1)
+
+    lineSettings
+    .add(settings, 'angle', 0.1, 10)
+    .onChange((value) => {
+      settings.angle = value
+    })
+    .step(0.1)
+
+    lineSettings
+    .add(settings, 'lineHeight', 0, 0.6)
+    .onChange((value) => {
+      settings.lineHeight = value
+    })
+    .step(0.1)
+
+  let a = 0.0
+  let inc = s.TWO_PI / 25.0
+  
+  const modulo = 15
+
+  const list_of_colors = ['skyblue', 'darkturquoise', 'aquamarine']
 
   s.setup = function () {
-    s.createCanvas(window.innerWidth, window.innerHeight);
-    // s.noLoop();
-    s.frameRate(1);
-  };
+    s.createCanvas(window.innerWidth, window.innerHeight)
+    s.frameRate(settings.fps)
+  }
 
   s.draw = () => {
-    s.background(0);
+    s.background(0)
 
-    let color_1, color_2, color;
+    let color_1, color_2, color
 
-    for (let i = 0; i < s.width; i++) {
-      if (i % 15 === 0) {
-        color_1 = s.random(list_of_colors);
-        color_2 = s.random(list_of_colors);
-        color = s.lerpColor(s.color(color_1), s.color(color_2), s.random());
+    for (let i = 0; i < s.width; i += settings.lineWidth) {
+      if (i % (settings.lineWidth * modulo) === 0) {
+        color_1 = s.random(list_of_colors)
+        color_2 = s.random(list_of_colors)
+        color = s.lerpColor(s.color(color_1), s.color(color_2), s.random())
       }
-      s.stroke(color);
-      s.strokeWeight(3);
-      s.line(
-        i * 4,
-        s.height / 2 + s.cos(a) * s.height * 0.3,
-        i * 4,
-        s.height / 2 + s.cos(a) * s.height * 0.3
-      );
 
-      a = a + inc * 0.5;
+      const posY = s.height / 2 + s.cos(a) * s.height * settings.lineHeight
+
+      s.stroke(color)
+      s.strokeWeight(6)
+      s.line(i, posY, i, posY)
+
+      a += inc * settings.angle
     }
-  };
+  }
 
   s.windowResized = () => {
-    s.resizeCanvas(s.windowWidth, s.windowHeight);
-  };
-};
+    s.resizeCanvas(s.windowWidth, s.windowHeight)
+  }
+}
 
-new p5(sketch);
+new p5(sketch)
